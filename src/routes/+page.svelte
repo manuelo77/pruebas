9e6898dc3d489d25893ac53befcd8e49955c467e
Manolo <manolo@Manolos-MacBook-Pro.local>
2023-04-25 17:32:32 -0400

    hola

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>html comun en este componente llamado page.svelte</h1>
+<img src="%sveltekit.assets%/favicon.png" alt="">
