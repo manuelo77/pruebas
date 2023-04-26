9483a0cd5335ae51945a552b39a2ebd17577ad9b
Manolo <manolo@Manolos-MacBook-Pro.local>
2023-04-26 18:52:09 -0400

    upgrade'i

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>html normal comun en este componente llamado page.svelte</h1>
-<img src="%sveltekit.assets%/favicon.png" alt="">
+<script> // script para importar el archivo Header.svelte que es la cabecera del sitio
+    import Header from './components/Header.svelte'; 
+    import Footer from './components/Footer.svelte';
+</script>
+
+<Header /> <!-- importa la cabecera por medio del script -->
+
+<main>
+    <p>MAIN</p>
+</main>
+
+<style>
+    main{
+        max-width: 960px;
+        margin: 40px auto;
+    }
+</style>
+
+<Footer /> <!-- importa el footer por medio del script -->
---
+++ src/routes/components/Header.svelte
@@ -0,0 +1,21 @@
+<header>
+    <h1>
+        <img src="/favicon.png" alt="favicon"> <!-- la carpeta donde esta esta imagen es STATIC en el root -->
+    </h1>
+    
+</header>
+
+<style>
+header{
+    background: #f7f7f7;
+    padding: 20px;
+}
+h1{
+    margin:0;
+    text-align: center;
+}
+img{
+    max-width: 180px;
+}
+
+</style>
